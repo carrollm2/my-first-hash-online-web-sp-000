1b4188c1d15fafe86f3edbc7dcdcb78060a53358
Michael Carroll <carrollm2@gmail.com>
2019-10-19 19:17:19 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -20,7 +20,7 @@ def retrieval
                  }
 
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-  return shipping_manifest["oil_paintings"]
+  return shipping_manifest["oil paintings"]
 end
 
 def adding
@@ -29,7 +29,7 @@ def adding
                   "porcelain vases" => 2,
                   "oil paintings" => 3,
                   "muskets" => 2,
-                  "gun_powder" => 4,
+                  "gun powder" => 4,
                  }
   # add 2 muskets to the shipping_manifest hash below
 
